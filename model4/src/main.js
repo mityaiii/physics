093997823404018f5d869dca242ae947c52ca440
mityaiii <pericle04@mail.ru>
2024-03-08 14:26:21 +0300

    added model4

---
+++ model4/src/main.js
@@ -0,0 +1,64 @@
+const drawGraphicEds = (data) => {
+  const layout = {
+    title: "ε(t)",
+    xaxis: {
+        title: 't, с',
+    },
+    yaxis: {
+        autorange: true,
+        title: 'ε, В',
+    }
+  };
+
+  Plotly.newPlot("EdsGrahic", data, layout);
+}
+
+const drawGraphicI = (data) => {
+  const layout = {
+    title: "I(t)",
+    xaxis: {
+        title: 't, с',
+    },
+    yaxis: {
+        autorange: true,
+        title: 'I, А',
+    }
+  };
+
+  Plotly.newPlot("IGraphic", data, layout);
+}
+
+const calculate = () => {
+  const eps = 0.001;
+
+  const b = parseFloat(document.getElementById("input-b").value);
+  const frequency = parseFloat(document.getElementById("input-frequency").value);
+  const resist = parseFloat(document.getElementById("input-resist").value);
+  const square = parseFloat(document.getElementById("input-square").value);
+
+  const tValues = [];
+  const IValues = [];
+  const EValues = [];
+
+  const frequencyAngle = 2 * Math.PI * frequency;
+
+  for (let t = 0; t < 10; t+=eps) {
+    tValues.push(t);
+
+    const E = b * frequencyAngle * square * Math.sin(frequencyAngle * t);
+    const I = E / resist;
+
+    EValues.push(E);
+    IValues.push(I);
+  }
+
+  const dataI = [{x: tValues, y: IValues, mode: "lines"}];
+  const dataE = [{x: tValues, y: EValues, mode: "lines"}];
+
+  drawGraphicEds(dataE);
+  drawGraphicI(dataI);
+
+}
+
+drawGraphicEds();
+drawGraphicI();
